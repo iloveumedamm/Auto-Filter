9021053934286988fdb46ec09e33782760f4c49f
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2024-02-05 20:20:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,9 +76,9 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'nboll')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/Rkbotzsupport')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/Rkbotz')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/RkMovie_group')
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/+8mOqChNoVPA3MDE1')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/streaamdb')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/+8mOqChNoVPA3MDE1')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/Rkbotz")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Rkbotz")
 
@@ -89,17 +89,17 @@ MAX_BTN = int(environ.get('MAX_BTN', 10)) #don't change anything in Language
 LANGUAGES = [language.lower() for language in environ.get('LANGUAGES', 'english hindi telugu tamil kannada malayalam').split()]
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 FILE_CAPTION = environ.get("FILE_CAPTION", script.FILE_CAPTION)
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "onepagelink.in")
-SHORTLINK_API = environ.get("SHORTLINK_API", "f646357aa129cfbd7eb59bcba428096ab54ca950")
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "krishnalink.com")
+SHORTLINK_API = environ.get("SHORTLINK_API", "3ecc329283a99d092c9125060b3138247b0a07e5")
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
 INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTENSIONS', 'mp4 mkv').split()]
 
 # boolean settings 
-GROUP_FSUB = is_enabled('GROUP_FSUB', False) 
-PM_SEARCH = is_enabled('PM_SEARCH', True) #switch True or False for searching results in bot pm😃
-IS_VERIFY = is_enabled('IS_VERIFY', False)
-AUTO_DELETE = is_enabled('AUTO_DELETE', False)
+GROUP_FSUB = is_enabled('GROUP_FSUB', 1) 
+PM_SEARCH = is_enabled('PM_SEARCH', 0) #switch True or False for searching results in bot pm😃
+IS_VERIFY = is_enabled('IS_VERIFY', 1)
+AUTO_DELETE = is_enabled('AUTO_DELETE', 1)
 WELCOME = is_enabled('WELCOME', False)
 PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
 LONG_IMDB_DESCRIPTION = is_enabled("LONG_IMDB_DESCRIPTION", False)
